ee9bea50bb2616b68a7333a024cbf3dc5dd3cad1
MohamedHalim <mohamedhalim1998@gmail.com>
2021-01-16 04:25:58 +0200

    ADD: allow note to save a color and time

--- lib/model/Note.dart
+++ lib/model/Note.dart
@@ -1,17 +1,28 @@
+
 class Note {
   String id;
   String title;
   String note;
+  int time;
+  int color;
 
-  Note({this.title, this.note});
+  Note({this.title, this.note, this.color});
 
   Note.fromMap(Map<String, dynamic> map) {
     this.id = map["id"];
     this.title = map["title"];
     this.note = map["note"];
+    this.time = map["time"];
+    this.color = map["color"];
   }
 
   Map<String, dynamic> toMap() {
-    return {"id": id, "title": title, "note": note};
+    return {
+      "id": id,
+      "title": title,
+      "note": note,
+      "time": time,
+      "color": color
+    };
   }
 }
--- lib/model/db_helper.dart
+++ lib/model/db_helper.dart
@@ -2,22 +2,25 @@ import 'package:flutter/foundation.dart';
 import 'package:notes_app/model/Note.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
-import 'package:uuid/uuid.dart';
 
 class DatabaseHelper with ChangeNotifier {
   Database database;
   String table = 'notes';
 
-
   init() async {
     database = await openDatabase(
       join(await getDatabasesPath(), "notes.db"),
-      onCreate: (db, version) =>
-          db.execute(
-              "CREATE TABLE $table(id TEXT PRIMARY KEY,title TEXT,note  TEXT);"),
-      version: 1,
+      onCreate: (db, version) => createTable(db),
+      onUpgrade: (db, oldVersion, newVersion) {
+        db.execute("DROP TABLE IF EXISTS $table;");
+        createTable(db);
+      },
+      version: 3,
     );
-    Note note = Note(title: "title", note: "NOTE");
+  }
+  void createTable(Database db){
+    db.execute(
+        "CREATE TABLE $table(id TEXT PRIMARY KEY,title TEXT,note  TEXT, color INTEGER, time INTEGER);");
   }
 
   Future<void> insert(Note note) async {
